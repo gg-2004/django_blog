e735ab6aaaf3df7f12ac6bf83b96ff8b06f33874
gg-2004 <138923116+gg-2004@users.noreply.github.com>
2025-09-16 09:51:53 +0530

    seed

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('create/', views.create_post, name='create_post'),
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path("seed/", views.seed_posts, name="seed_posts"),
+
 
 ]
