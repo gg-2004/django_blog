1cdb079e7600a299a1fe12ccfaa05f52653e4bbc
gg-2004 <138923116+gg-2004@users.noreply.github.com>
2025-09-16 11:41:51 +0530

    new

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('create/', views.create_post, name='create_post'),
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
-    path("setup-portfolio/", views.setup_portfolio),
+    
 
 
     
